6ff4d0f693260ddc995ae5c988c3607c2fffa1fd
Yvon Viger <yvon@bayteksystems.com>
2016-01-22 11:37:26 -0500

    Adding the code required for the status bit

---
+++ StatusTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Laravel StatusBit package.
+ *
+ * (c) Yvon Viger <yvon@baytek.ca>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Baytek;
+
+trait StatusTrait {
+
+	/**
+     * @param Builder $query Elequont query builder
+     * @param Integer $statuses Statuses
+	 * @return Builder return the builder back
+	 */
+	public function scopeIncludeStatus($query, $statuses)
+    {
+    	if(!is_array($statuses)) $statuses = [$statuses];
+
+    	foreach($statuses as $status) {
+    		$query = $query->whereRaw("status & {$status} != 0");
+    	}
+        return $query;
+    }
+
+    /**
+     * @param Builder $query Elequont query builder
+     * @param Integer $statuses Statuses
+     * @return Builder return the builder back
+     */
+	public function scopeExcludeStatus($query, $statuses)
+    {
+        if(!is_array($statuses)) $statuses = [$statuses];
+
+    	foreach($statuses as $status) {
+    		$query = $query->whereRaw("status & {$status} = 0");
+    	}
+        return $query;
+    }
+}
+
+
