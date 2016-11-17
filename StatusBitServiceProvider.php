447089dc51f8cc1712e5d9c62dea7838423d382e
Yvon Viger <yvon@bayteksystems.com>
2016-11-17 16:39:05 -0500

    Finalizing the status histories stuff

--- StatusBitServiceProvider.php
+++ StatusBitServiceProvider.php
@@ -14,10 +14,12 @@ class StatusBitServiceProvider extends ServiceProvider {
      */
     public function boot()
     {
-        collect(config('status.history_models'))->each(function ($model)
-        {
-            forward_static_call([$model, 'observe'], StatusObserver::class);
-        });
+        if(config('status.use_histories')) {
+            collect(config('status.history_models'))->each(function ($model)
+            {
+                forward_static_call([$model, 'observe'], StatusObserver::class);
+            });
+        }
 
         $this->publishes([
             __DIR__.'/config/status.php' => config_path('status.php'),
--- StatusHistory.php
+++ StatusHistory.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class StatusHistory extends Model
 {
     protected $fillable = ['type_id', 'type', 'status'];
+
+    public function __construct(array $attributes = [])
+    {
+    	// Set the table used for the status histories
+    	$this->setTable(config('status.history_table'));
+
+    	parent::__construct($attributes);
+    }
+
 }
