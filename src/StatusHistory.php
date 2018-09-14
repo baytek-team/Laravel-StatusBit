179b4d7e176757ee4597c46cef797b20d5f242a7
Sarah Marinoff <sarah@baytek.ca>
2018-09-14 14:39:25 -0400

    have state be fillable, so it can actually be recorded…

--- src/StatusHistory.php
+++ src/StatusHistory.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class StatusHistory extends Model
 {
-    protected $fillable = ['type_id', 'type', 'status', 'comment'];
+    protected $fillable = ['type_id', 'type', 'status', 'comment', 'state'];
 
     public function __construct(array $attributes = [])
     {
